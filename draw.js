844fa5f6b625874e0f3695d47553c4d29427c663
Adam Pearce <1wheel@gmail.com>
2016-07-06 00:39:03 -0400

    triangle working

--- draw.js
+++ draw.js
@@ -7,15 +7,62 @@ var height = 960
 var canvas = d3.select('canvas').node()
 var ctx = canvas.getContext('2d');
 
-ctx.beginPath()
-ctx.moveTo(0, height)
-ctx.lineTo(width/2, 0)
-ctx.stroke()
+ctx.clearRect(0, 0, width, height)
+
+ctx.strokeStyle = '#fff'
+
+
+function drawOutline(){
+  ctx.beginPath()
+  ctx.moveTo(0, height)
+  ctx.lineTo(width/2, 0)
+  ctx.stroke()
+
+  ctx.beginPath()
+  ctx.moveTo(width, height)
+  ctx.lineTo(width/2, 0)
+  ctx.stroke()
+}
+
+function drawTriangle(p){
+  ctx.beginPath()
+  ctx.lineTo(p[0][0], p[0][1])
+  ctx.lineTo(p[1][0], p[1][1])
+  ctx.lineTo(p[2][0], p[2][1])
+  ctx.fill()
+}
+
+
+
+ctx.fillStyle = 'rgba(255,255,255,.04)'
+
+var points = d3.range(200).map(function(d, i){
+  return [randomL(), randomR(), i % 2 ? randomL() : randomR()]
+})
+
+d3.timer(function(d){
+  ctx.clearRect(0, 0, width, height)
+
+  drawOutline()
+
+  points.forEach(function(p, i){
+    var p = [randomL(), randomR(), i % 2 ? randomL() : randomR()]
+    drawTriangle(p)
+  })
+
+})
+
+
+function randomL(){
+  var x = Math.random()*width/2
+  return [x, height - x*2]
+}
+
+function randomR(){
+  var x = Math.random()*width/2 + width/2
+  return [x, x*2- height]
+}
 
 
 console.log('drawing')
 
-
-// ctx.fillStyle = "#" + formatHex(i) + "0000";
-// ctx.fill();
-// ctx.stroke()
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require("./style.css");
+var d3 = require("./d3v4+jetpack.js");
+
 
 var draw = require("./draw.js");
 
